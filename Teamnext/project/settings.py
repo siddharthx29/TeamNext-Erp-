102dd720cbcef9dcd0cd3640550b27fa5c233fa5
Siddharth MV <siddharthmv123@gmail.com>
2026-03-10 03:10:12 +0530

    initial commit

--- Teamnext/project/settings.py
+++ Teamnext/project/settings.py
@@ -114,14 +114,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email Settings
 if 'EMAIL_HOST' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = os.environ.get('EMAIL_HOST')
-    EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
-    EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False').lower() == 'true'
-    EMAIL_TIMEOUT = 10  # Stop waiting after 10 seconds (prevents Internal Server Error)
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = os.getenv("EMAIL_HOST")
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+    EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
+    EMAIL_TIMEOUT = 10
 else:
     # Outputs emails to the console for local development
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
