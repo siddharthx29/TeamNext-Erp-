861eeb6701761165dc89cbd5fe855d19cc516ef1
Siddharth MV <siddharthmv123@gmail.com>
2026-03-10 03:36:51 +0530

    initial commit

--- Teamnext/project/settings.py
+++ Teamnext/project/settings.py
@@ -113,18 +113,14 @@ CSRF_TRUSTED_ORIGINS = ['https://*.railway.app', 'https://*.onrender.com', 'http
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email Settings
-if 'EMAIL_HOST' in os.environ:
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = os.getenv("EMAIL_HOST")
-    EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
-    EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
-    EMAIL_TIMEOUT = 10
-else:
-    # Outputs emails to the console for local development
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
+EMAIL_TIMEOUT = 10
 
 # Show errors in terminal when DEBUG=False
 LOGGING = {
