9ff297b5991fc4c8b1dc043045885ec7970d63da
Siddharth MV <siddharthmv123@gmail.com>
2026-03-10 04:08:19 +0530

    initial commit

--- Teamnext/project/settings.py
+++ Teamnext/project/settings.py
@@ -114,13 +114,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email Settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
-EMAIL_TIMEOUT = 10
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 # Show errors in terminal when DEBUG=False
 LOGGING = {
