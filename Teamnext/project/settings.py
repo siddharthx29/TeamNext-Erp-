e4ac66a1eb5e1be8451b85e95af98cd4aef99359
Siddharth MV <siddharthmv123@gmail.com>
2026-03-10 01:39:59 +0530

    main

--- Teamnext/project/settings.py
+++ Teamnext/project/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'myapp' / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
