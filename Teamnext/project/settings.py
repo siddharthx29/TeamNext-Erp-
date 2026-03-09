9643fdf9b99cd709d8e25ca3791dfa1a5469b60e
Siddharth MV <siddharthmv123@gmail.com>
2026-03-09 22:40:46 +0530

    main

--- Teamnext/project/settings.py
+++ Teamnext/project/settings.py
@@ -104,7 +104,7 @@ STATICFILES_DIRS = []
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app', 'https://*.onrender.com']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
