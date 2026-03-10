1d3ac9f7f4eb7dc2e8a460b69038a2cd6253c997
Siddharth MV <siddharthmv123@gmail.com>
2026-03-10 10:34:06 +0530

    main1

--- Teamnext/project/settings.py
+++ Teamnext/project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-temp-key-change-this')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') != 'False'
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
