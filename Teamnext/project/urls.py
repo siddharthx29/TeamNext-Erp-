e00760664675c16af10d5d93fabb6107a458fe24
Siddharth MV <siddharthmv123@gmail.com>
2026-03-14 02:30:24 +0530

    main

--- Teamnext/project/urls.py
+++ Teamnext/project/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-
 from django.urls import path, include
+from django.http import HttpResponse
+
+def google_verify(request):
+    return HttpResponse("google-site-verification: google05aabfcd698e02fd.html", content_type="text/html")
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
-
+    path("google05aabfcd698e02fd.html", google_verify),
     path('', include('myapp.urls')),
-
 ]
